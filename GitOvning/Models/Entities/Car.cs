bd4c369f704010c7bc5fb00dc91129e9c300207e
buffelsafari <buffelsafari@hotmail.com>
2021-08-18 17:53:32 +0200

    test

---
+++ GitOvning/Models/Entities/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitOvning.Models.Entities
+{
+    public class Car
+    {
+        public int Id { get; set; }
+    }
+}
