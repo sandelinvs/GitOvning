03abfd64911acf65e454f945a7dd37afa7697fc2
sandelinvs@outlook.com <sandelinvs@outlook.com>
2021-08-18 15:36:41 +0200

    skoj

--- GitOvning/Program.cs
+++ GitOvning/Program.cs
@@ -13,7 +13,7 @@ namespace GitOvning
     {
         public static void Main(string[] args)
         {
-        // Här är Artems commit tobbes
+        // Här är Artems commit tobbes mer?
             CreateHostBuilder(args).Build().Run();
         }
 
