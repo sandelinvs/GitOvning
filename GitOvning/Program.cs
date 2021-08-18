8dddf9dce9ed257042af2b8830779de97d9ce3d2
sandelinvs@outlook.com <sandelinvs@outlook.com>
2021-08-18 15:34:35 +0200

    tobbes konflikt

--- GitOvning/Program.cs
+++ GitOvning/Program.cs
@@ -13,7 +13,7 @@ namespace GitOvning
     {
         public static void Main(string[] args)
         {
-        // Här är Artems commit
+        // Här är Artems commit tobbes
             CreateHostBuilder(args).Build().Run();
         }
 
